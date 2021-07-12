d372620ab4afb6e38ad90ed8511b6e9e7027b8d9
Tai Doan <doanchitaidn@gmail.com>
2021-07-12 14:15:39 +0700

    Add feature update profile user and fix some stuff

--- resources/js/pages/auth/Profile.vue
+++ resources/js/pages/auth/Profile.vue
@@ -6,7 +6,11 @@
             <div class="card">
                 <div class="card-header">Update profile</div>
                 <div class="card-body">
-                    <form action="#" method="POST" id="card-form">
+                    <form
+                        action="#"
+                        method="POST"
+                        @submit.prevent="handleSubmit"
+                    >
                         <div class="form-group">
                             <label
                                 for="coupon"
@@ -18,8 +22,19 @@
                                 type="text"
                                 name="name"
                                 id="name"
+                                v-model="form.name"
+                                :class="{
+                                    'is-invalid': errors && errors.name,
+                                }"
                                 class="form-control"
                             />
+                            <span
+                                class="invalid-feedback"
+                                role="alert"
+                                v-if="errors && errors.name"
+                            >
+                                <strong>{{ errors.name[0] }}</strong>
+                            </span>
                         </div>
                         <div class="form-group">
                             <label
@@ -33,7 +48,62 @@
                                 type="text"
                                 name="email"
                                 id="email"
+                                v-model="form.email"
+                                :class="{
+                                    'is-invalid': errors && errors.email,
+                                }"
                                 class="form-control"
+                            />
+                            <span
+                                class="invalid-feedback"
+                                role="alert"
+                                v-if="errors && errors.email"
+                            >
+                                <strong>{{ errors.email[0] }}</strong>
+                            </span>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password" class="col-form-label">
+                                Password:
+                            </label>
+
+                            <input
+                                id="password"
+                                type="password"
+                                class="form-control"
+                                v-model="form.password"
+                                :class="{
+                                    'is-invalid': errors && errors.password,
+                                }"
+                            />
+
+                            <span
+                                class="invalid-feedback"
+                                role="alert"
+                                v-if="errors && errors.password"
+                            >
+                                <strong>{{ errors.password[0] }}</strong>
+                            </span>
+                        </div>
+
+                        <div class="form-group">
+                            <label
+                                for="password-confirm"
+                                class="col-form-label"
+                            >
+                                Confirm Password:
+                            </label>
+
+                            <input
+                                id="password-confirm"
+                                type="password"
+                                class="form-control"
+                                :class="{
+                                    'is-invalid':
+                                        errors && errors.password_confirmation,
+                                }"
+                                v-model="form.password_confirmation"
                             />
                         </div>
 
@@ -52,15 +122,48 @@
 
 <script>
     import Sidebar from '@/layouts/Sidebar';
+    import { mapActions, mapGetters } from 'vuex';
     export default {
         components: { Sidebar },
+        computed: {
+            ...mapGetters({
+                user: 'auth/user',
+            }),
+        },
         data() {
             return {
                 form: {
                     name: '',
                     email: '',
+                    password: '',
+                    password_confirmation: '',
                 },
+                errors: null,
             };
+        },
+        methods: {
+            ...mapActions({
+                updateProfile: 'auth/updateProfile',
+            }),
+            handleSubmit() {
+                this.updateProfile(this.form)
+                    .then(() => {
+                        toastr.success('Updated successful !', 'Success!');
+                        this.errors = null;
+                        this.form.password = '';
+                        this.form.password_confirmation = '';
+                    })
+                    .catch(error => {
+                        this.errors = error.response.data.errors;
+                    });
+            },
+        },
+        mounted() {
+            if (this.user) {
+                const { name, email } = this.user;
+                this.form.name = name;
+                this.form.email = email;
+            }
         },
     };
 </script>
